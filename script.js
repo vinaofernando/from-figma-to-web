2404cb634130117d1abb95f1ab669633f599b1d4
Vinicius Fernando <viniciush.fernando@hotmail.com>
2021-12-01 10:26:10 +0000

    remove js

--- script.js
+++
@@ -1,9 +0,0 @@
-const menuList = document.getElementById("menu-checkbox");
-function visualList() {
-  const menuNav = document.getElementById("links");
-  const buttonSelect = document.getElementById("group-bottons");
-
-  buttonSelect.classList.toggle("active");
-  menuNav.classList.toggle("active");
-}
-menuList.addEventListener("click", visualList);
